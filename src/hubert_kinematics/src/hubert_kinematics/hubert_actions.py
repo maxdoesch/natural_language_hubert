ad22d9a3f95cb0e0bc304dcf10f0ff16cbd3b27d
Lucas Kristiansson <lucas.kristiansson@gmail.com>
2024-10-17 11:22:46 +0200

    Added hardcoded stances for hubert

---
+++ src/hubert_kinematics/src/hubert_kinematics/hubert_actions.py
@@ -0,0 +1,36 @@
+from angle2pcm import Angle2pcm as _Angle2pcm
+from pcm2angle import Pcm2angle as _Pcm2angle
+import numpy as np
+from math import pi
+
+angle2pcm = _Angle2pcm()
+pcm2angle = _Pcm2angle()
+
+class Hubert:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def gripper_open():
+        """Returns the pcm value that corresponds to a fully opened gripper"""
+        return angle2pcm.gripper(33)
+    
+    @staticmethod
+    def gripper_close():
+        """Returns the pcm value that corresponds to a fully closed gripper"""
+        return angle2pcm.gripper(0)
+    
+    @staticmethod
+    def arm_idle():
+        """Returns the pcm values for [shoulder joint, elbow joint] for idle position in arms"""
+        return np.array([angle2pcm.shoulder(3*pi/2), angle2pcm.elbow(-pi/2)])
+    
+    def arm_lift():
+        """DO NOT USE"""
+        pass
+    
+    @staticmethod
+    def head_tilt_down():
+        """Returns the pcm value that corresponds to the angle of looking down towards the workspace"""
+        return angle2pcm.head_tilt(1300)
+    
--- src/hubert_kinematics/src/hubert_kinematics/pcm2angle.py
+++ src/hubert_kinematics/src/hubert_kinematics/pcm2angle.py
@@ -3,102 +3,125 @@ PI = 3.1415926536
 
 print("pcm2angle.py is being executed.")
 
-def __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max) -> float:
-    # Raise error if PCM is outside of the boundary
-    if pcm < pcm_min or pcm > pcm_max:
-        raise ValueError(f"PCM must be between {pcm_min} and {pcm_max}.")
+class Pcm2angle:
+    def __init__(self):
+        pass
 
-    # Linear interpolation from PCM to angle
-    angle_value = angle_min + (pcm - pcm_min) * (angle_max - angle_min) / (pcm_max - pcm_min)
+    @staticmethod
+    def __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max) -> float:
+        # Raise error if PCM is outside of the boundary
+        if pcm < pcm_min or pcm > pcm_max:
+            raise ValueError(f"PCM must be between {pcm_min} and {pcm_max}.")
 
-    return -angle_value  # Return the angle as a float
+        # Linear interpolation from PCM to angle
+        angle_value = angle_min + (pcm - pcm_min) * (angle_max - angle_min) / (pcm_max - pcm_min)
 
-def body(pcm: int) -> float:
-    """
-    Function that converts PCM for BODY servo to angle.
+        return -angle_value  # Return the angle as a float
+
+    def body(self, pcm: int) -> float:
+        """
+        Function that converts PCM for BODY servo to angle.
+        
+        :param pcm: PCM value [560, 2330]
+        :return: Corresponding angle in radians.
+        """
+
+        angle_min = -PI/2
+        angle_max = PI/2
+
+        pcm_min = 560
+        pcm_max = 2330
+
+        angle_value = self.__calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
+
+        return angle_value  # Return as a float
+
+    def head_pan(self, pcm: int) -> float:
+        """
+        Function that converts PCM for HEAD_PAN servo to angle.
+        
+        :param pcm: PCM value [550, 2400]
+        :return: Corresponding angle in radians.
+        """
+
+        angle_min = -PI/2
+        angle_max = PI/2
+
+        pcm_min = 550
+        pcm_max = 2400
+
+        angle_value = self.__calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
+
+        return angle_value  # Return as a float
+
+    def head_tilt(self, pcm: int) -> float:
+        """
+        Function that converts PCM for HEAD_TILT servo to angle.
+        
+        :param pcm: PCM value [950, 2400]
+        :return: Corresponding angle in radians.
+        """
+
+        angle_min = -PI/4
+        angle_max = PI/2
+
+        pcm_min = 950
+        pcm_max = 2400
+
+        angle_value = self.__calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
+
+        return angle_value  # Return as a float
+
+    def shoulder(self, pcm: int) -> float:
+        """
+        Function that converts PCM for SHOULDER servo to angle.
+        
+        :param pcm: PCM value [750, 2200]
+        :return: Corresponding angle in radians.
+        """
+
+        angle_min = -PI/6
+        angle_max = PI/2
+
+        pcm_min = 750
+        pcm_max = 2200
+
+        angle_value = self.__calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
+
+        return -angle_value  # Return as a float
+
+    def elbow(self, pcm: int) -> float:
+        """
+        Function that converts PCM for ELBOW servo to angle.
+        
+        :param pcm: PCM value [550, 2400]
+        :return: Corresponding angle in radians.
+        """
+
+        angle_min = -PI/2
+        angle_max = PI/2
+
+        pcm_min = 550
+        pcm_max = 2400
+
+        angle_value = self.__calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
+
+        return -angle_value  # Return as a float
     
-    :param pcm: PCM value [560, 2330]
-    :return: Corresponding angle in radians.
-    """
+    def gripper(self, pcm: int) -> float:
+        """
+        Function that converts PCM for GRIPPER servo to translation.
+        
+        :param pcm: PCM value [550, 2150]
+        :return: Corresponding distance from 0 - 33 [mm].
+        """
 
-    angle_min = -PI/2
-    angle_max = PI/2
+        dist_min = 0
+        dist_max = 33
 
-    pcm_min = 560
-    pcm_max = 2330
+        pcm_min = 550
+        pcm_max = 2150
 
-    angle_value = __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
+        dist_value = self.__calculate(pcm, dist_min, dist_max, pcm_min, pcm_max)
 
-    return angle_value  # Return as a float
-
-def head_pan(pcm: int) -> float:
-    """
-    Function that converts PCM for HEAD_PAN servo to angle.
-    
-    :param pcm: PCM value [550, 2400]
-    :return: Corresponding angle in radians.
-    """
-
-    angle_min = -PI/2
-    angle_max = PI/2
-
-    pcm_min = 550
-    pcm_max = 2400
-
-    angle_value = __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
-
-    return angle_value  # Return as a float
-
-def head_tilt(pcm: int) -> float:
-    """
-    Function that converts PCM for HEAD_TILT servo to angle.
-    
-    :param pcm: PCM value [950, 2400]
-    :return: Corresponding angle in radians.
-    """
-
-    angle_min = -PI/4
-    angle_max = PI/2
-
-    pcm_min = 950
-    pcm_max = 2400
-
-    angle_value = __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
-
-    return angle_value  # Return as a float
-
-def shoulder(pcm: int) -> float:
-    """
-    Function that converts PCM for SHOULDER servo to angle.
-    
-    :param pcm: PCM value [750, 2200]
-    :return: Corresponding angle in radians.
-    """
-
-    angle_min = -PI/6
-    angle_max = PI/2
-
-    pcm_min = 750
-    pcm_max = 2200
-
-    angle_value = __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
-
-    return -angle_value  # Return as a float
-
-def elbow(pcm: int) -> float:
-    """
-    Function that converts PCM for ELBOW servo to angle.
-    
-    :param pcm: PCM value [550, 2400]
-    :return: Corresponding angle in radians.
-    """
-
-    angle_min = -PI/2
-    angle_max = PI/2
-
-    pcm_min = 550
-    pcm_max = 2400
-
-    angle_value = __calculate(pcm, angle_min, angle_max, pcm_min, pcm_max)
-
-    return -angle_value  # Return as a float
+        return dist_value  # Return as a float
