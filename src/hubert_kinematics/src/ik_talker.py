9bc42513d32fc36857f2ee1a51e3162901116432
adri1cc <adrien.jacquet10@gmail.com>
2024-10-17 11:21:50 +0200

    First movement

--- src/hubert_kinematics/src/ik_talker.py
+++ src/hubert_kinematics/src/ik_talker.py
@@ -8,7 +8,7 @@ import time
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Point
 
-from hubert_kinematics import pcm2angle
+from hubert_kinematics import angle2pcm
 
 from hubert_launch.msg import LabeledPoint
 
@@ -17,6 +17,32 @@ from hubert_kinematics.ik_solver import InverseKinematics as IK
 from math import pi
 import random
 import numpy as np
+
+PI = 3.1415926536
+
+class Listener:
+    def __init__(self):
+        self.sub_label = rospy.Subscriber("/label_topic", String, self.label_callback)
+        self.sub_coords = rospy.Subscriber("/coordinates", LabeledPoint, self.coordinates_callback)
+        self.coordinates_received = False
+        self.label_received = False
+    
+    def label_callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+        self.label = str(data.data)
+        print(self.label)
+        self.label_received = True
+        self.coordinates_received = False
+
+    def coordinates_callback(self, data):
+        
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s, %s, %s, %s", data.point.x, data.point.y, data.point.z, data.label)
+        rospy.loginfo(rospy.get_caller_id() + "I want to hear %s", self.label)
+        if data.label == self.label:
+            self.coordinates_received = True
+            print('fjdskljfkls')
+        else:
+            self.coordinates_received = False
 
 def create_joint_state_msg(positions):
     msg = JointState()
@@ -30,15 +56,39 @@ def create_joint_state_msg(positions):
 
 def joints_talker():
 
+    sub_listener = Listener()
+
     pub_joint_states = rospy.Publisher('/joint_states', JointState, queue_size=10)
 
-    rospy.init_node('rviz_joints_talker', anonymous=True)
+    pub_body = rospy.Publisher('/servo_body', UInt16, queue_size=10, latch=True)
+    pub_neck_tilt = rospy.Publisher('/servo_neck_tilt', UInt16, queue_size=10, latch=True)
+    pub_shoulder = rospy.Publisher('/servo_shoulder', UInt16, queue_size=10)
+    pub_elbow = rospy.Publisher('/servo_elbow', UInt16, queue_size=10)
+
+    rospy.init_node('joints_talker', anonymous=True)
 
     rate = rospy.Rate(10) # 10hz
+
+    positions = [0, 0, 0, -PI/4, PI/2]
+    msg = create_joint_state_msg(positions)
+    pub_joint_states.publish(msg)
+
+    elbow_first_value = angle2pcm.elbow(-PI/4)
+    shoulder_first_value = angle2pcm.shoulder(PI/2)
+
+    rospy.loginfo(elbow_first_value)
+    pub_elbow.publish(elbow_first_value)
+    time.sleep(2)
+    rospy.loginfo(shoulder_first_value)
+    pub_shoulder.publish(shoulder_first_value)
+
+
 
     theta1 = random.uniform(-pi/2, pi/2)
     theta2 = random.uniform(pi/6, pi/2)
     theta3 = random.uniform(-pi/2, 0)
+
+    time.sleep(10)
 
     print(f"Angles chosen for theta1, theta2 and theta3: {theta1}, {theta2}, {theta3}")
 
@@ -52,6 +102,8 @@ def joints_talker():
     print(f"Final angles given to Hubert for theta1, theta2 and theta3: {angles[0]}, {angles[1]}, {angles[2]}")
 
     pub_joint_states.publish(msg)
+
+    rate.sleep()
 
 
 if __name__ == '__main__':
