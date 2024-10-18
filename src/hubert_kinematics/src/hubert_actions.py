45e44c53e6980d653e411c54a21826a4b7caa513
Lucas Kristiansson <lucas.kristiansson@gmail.com>
2024-10-18 12:24:16 +0200

    more changes tothe hubert script

--- src/hubert_kinematics/src/hubert_actions.py
+++ src/hubert_kinematics/src/hubert_actions.py
@@ -9,86 +9,123 @@ angle2pcm = _Angle2pcm()
 pcm2angle = _Pcm2angle()
 
 class Hubert:
-    def __init__(self, joint_publisher_list, jointstate_publisher):
+    def _init_(self):
         """
         Hubert stance class
 
-        Parameters
-        ----------
-        joint_publisher_list : list
-            List of every publisher as [body, neck_tilt, neck_pan, shoulder, elbow, gripper]
-        jointstate_publisher : rospy.Publisher
-            The joint states publisher
+        Gives pcm values (or angles) for given tasks. 
+        Methods:
+        --------
+        stance - Returns every joint values for a stance
+        arm - Returns three joints (body, shoulder, elbow) for a stance
+
         """
 
         # PCM values for each joint
-        self.pcm_body = 0
-        self.pcm_neck_tilt = 0
-        self.pcm_neck_pan = 0
-        self.pcm_shoulder = 0
-        self.pcm_elbow = 0
-        self.pcm_gripper = 0
+        self._pcm_body = 0
+        self._pcm_neck_tilt = 0
+        self._pcm_neck_pan = 0
+        self._pcm_shoulder = 0
+        self._pcm_elbow = 0
+        self._pcm_gripper = 0
         
         # Angle values for each joint
-        self.angle_body = 0
-        self.angle_neck_tilt = 0
-        self.angle_neck_pan = 0
-        self.angle_shoulder = 0
-        self.angle_elbow = 0
-        self.angle_gripper = 0
-    
+        self._angle_body = 0
+        self._angle_neck_tilt = 0
+        self._angle_neck_pan = 0
+        self._angle_shoulder = 0
+        self._angle_elbow = 0
+        self._angle_gripper = 0
+
+    def _update_pcm_positions(self):
+        self._pcm_body = angle2pcm.body(self._angle_body)
+        self._pcm_neck_tilt = angle2pcm.neck_tilt(self._angle_neck_tilt)
+        self._pcm_neck_pan = angle2pcm.neck_pan(self._angle_neck_pan)
+        self._pcm_shoulder = angle2pcm.shoulder(self._angle_shoulder)
+        self._pcm_elbow = angle2pcm.elbow(self._angle_elbow)
+        self._pcm_gripper = angle2pcm.gripper(self._angle_gripper)
+
+    def _update_angle_positions(self):
+        self._angle_body = pcm2angle.body(self._pcm_body)
+        self._angle_neck_tilt = pcm2angle.head_tilt(self._pcm_neck_tilt)
+        self._angle_neck_pan = pcm2angle.head_pan(self._pcm_neck_pan)
+        self._angle_shoulder = pcm2angle.shoulder(self._pcm_shoulder)
+        self._angle_elbow = pcm2angle.elbow(self._pcm_elbow)
+        self._angle_gripper = pcm2angle.gripper(self._pcm_gripper)
+
     def get_jointstate(self):
-        """Updates the positions of the Hubert robot and publishes the joint states"""
-        positions_angle = [self.angle_body, self.angle_neck_tilt, self.angle_neck_pan, self.angle_shoulder, self.angle_elbow]
+        """
+        Return necessary positions for publishing the joint states
+
+        Returns
+        --------
+        positions_angle : list
+            [body, neck_tilt, neck_pan, shoulder, elbow]
+        """
+        positions_angle = [self._angle_body, self._angle_neck_tilt, self._angle_neck_pan, self._angle_shoulder, self._angle_elbow]
         return positions_angle
 
-    def move_arm(self, coordinates):
-
-        inverse_kinematics = IK(coordinates[0], coordinates[1], coordinates[2])
-        [self.angle_body, self.angle_shoulder, self.angle_elbow] = inverse_kinematics.angles
-        self.pcm_body = angle2pcm.body(self.angle_body)
-        self.pcm_shoulder = angle2pcm.shoulder(self.angle_body)
-        self.pcm_elbow = angle2pcm.elbow(self.angle_elbow)
-
-        self.update_positions()
-
-    @staticmethod
-    def gripper_open():
+    def get_gripper_open(self):
         """Returns the pcm value that corresponds to a fully opened gripper"""
-        return angle2pcm.gripper(33)
+        self._angle_gripper = 33
+        self._update_pcm_positions()
+
+        return self._pcm_gripper
     
-    @staticmethod
-    def gripper_close():
+    def get_gripper_close(self):
         """Returns the pcm value that corresponds to a fully closed gripper"""
-        return angle2pcm.gripper(0)
+        self._angle_gripper = 0
+        self._update_pcm_positions()
+
+        return self._pcm_gripper
     
-    @staticmethod
-    def arm_idle():
+    def get_arm_idle(self):
         """Returns the pcm values for [shoulder, elbow] for idle position in arms"""
         return np.array([angle2pcm.shoulder(pi/2), angle2pcm.elbow(-pi/2)])
+
+    def get_arm_goto(self, coordinates: list):
+        """
+        Calculate eef position and return relevant angles for execution
+        
+        Parameters
+        ----------
+        coordinates : list
+            A list of coordinates in [x, y, z] with float values
+
+        Return
+        ----------
+        pcm_list : list
+            A list of the pcm values for [body, shoulder, elbow]
+        """
+
+        inverse_kinematics = IK(coordinates[0], coordinates[1], coordinates[2])
+        [self._angle_body, self._angle_shoulder, self._angle_elbow] = inverse_kinematics.angles
+
+        self._update_pcm_positions()
+        return [self._pcm_body, self._pcm_shoulder, self._pcm_elbow]
+
     
-    def arm_lift():
-        """DO NOT USE"""
-        pass
-    
-    @staticmethod
-    def neck_tilt_down():
+    def get_neck_tilt_down(self):
         """Returns the pcm value that corresponds to the angle of looking down towards the workspace"""
         return 1300 # Value is hardcoded
     
-    @staticmethod
-    def stance_first(return_angles=False):
-        """Returns a fully idle stance with 0 angles on all servos
+
+    def get_stance_first(self):
+        """
+        Returns a fully idle stance with 0 angles on all servos and an open gripper
         
-        Returns [body, neck_tilt, neck_pan, shoulder, elbow, gripper]
+        Return
+        -------
+        pcm_values : list
+            [body, neck_tilt, neck_pan, shoulder, elbow, gripper]
 
         """
-        return np.array([
+        [
             angle2pcm.body(),
             angle2pcm.neck_tilt(),
             angle2pcm.neck_pan(),
             angle2pcm.shoulder(),
             angle2pcm.elbow(),
             angle2pcm.gripper(33)
-        ])
+        ]
 
