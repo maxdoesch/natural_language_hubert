aa4d049553c9bc5fa4d2ff4054d89264b65785bd
maxdoesch <doesch.maximilian@gmail.com>
2024-10-21 15:17:52 +0200

    updated prompt

--- src/hubert_natural_language/scripts/hubert_natural_language.py
+++ src/hubert_natural_language/scripts/hubert_natural_language.py
@@ -1,19 +1,28 @@
 from openai import OpenAI
 client = OpenAI()
 
-PROMPT_SYSTEM = "You are a stationary robot arm tasked with executing specific actions based on a given query and an image of the scene in front of you. \
-    Your responses should be a detailed list of atomic instructions necessary to complete the task. \
-    You can only use the following instructions: 'place arm over <object>; close end effector; open end effector; move arm by <x,y> milimeters relative to ground plane'"
+PROMPT_SYSTEM = "You are a stationary robot arm tasked with executing instructions based on a given query and an image of the scene in front of you. \
+    In order to be able execute these instructions you break them down into smaller atomic instructions which are necessary to complete the task. \
+    You can only use the following set of instructions: (place arm over <object>; perform grabbing action with end effector; open end effector; move arm by <x,y> millimeters relative to the ground plane) \
+    Any other instruction not provided in this set is illegal!"
 
-PROMPT_USER = "Please prepare me some apple slices."
+PROMPT_USER_1 = "Please prepare me some apple slices. + <An image of a cutting board, which has an apple and a knive placed on it>"
 
-PROMPT_ASSISTANT = "1. open end effector \
+PROMPT_ASSISTANT_1 = "1. open end effector \
                     2. place arm over <knive> \
-                    3. close end effector \
+                    3. perform grabbing action with end effector \
                     4. place arm over <apple> \
-                    5. move arm by <20, 0> millimeter relative to ground plane \
-                    6. move arm by <-20, 0> millimeter relative to ground plane \
-                    7. move arm by <20, 0> millimeter relative to ground plane"
+                    5. move arm by <20,y> millimeters relative to the ground plane \
+                    6. move arm by <-20,y> millimeters relative to the ground plane \
+                    7. move arm by <20,y> millimeters relative to the ground plane"
+
+PROMPT_USER_2 = "Bring my child something to play with. + <An image of a nursery containing a green ball, a stuffed giraffe a table and a shelve>"
+
+PROMPT_ASSISTANT_2 = "1. open end effector \
+                    2. place arm over <stuffed giraffe> \
+                    3. perform grabbing action with end effector \
+                    4. place arm over <child> \
+                    5. open end effector"
 
 completion = client.chat.completions.create(
     model="gpt-4o",
@@ -24,16 +33,14 @@ completion = client.chat.completions.create(
         },
         {
         "role": "user",
-        "content": [{ "type": "text", "text": PROMPT_USER},
-                    {"type": "image_url", "image_url": {"url": "https://www.shutterstock.com/shutterstock/photos/175132970/display_1500/stock-photo-kitchen-knife-and-green-apple-on-wooden-background-175132970.jpg", "detail": "low"},}
-                ]
+        "content": [{ "type": "text", "text": PROMPT_USER_1},]
         },
         {
         "role": "assistant",
-        "content": [{ "type": "text", "text": PROMPT_ASSISTANT}]
+        "content": [{ "type": "text", "text": PROMPT_ASSISTANT_1}]
         },
         {"role": "user",
-         "content": [{"type": "text", "text": "Bring my child something to play."},
+         "content": [{"type": "text", "text": "I want to play soccer!"},
                      {"type": "image_url", "image_url": {"url": "https://www.coasterfurniture.com/wp-content/uploads/nursery-playroom.jpeg", "detail": "low"},}
                 ]
         }
